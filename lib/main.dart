06fdfece690c30803ce8535273c0ecb8f37f7f48
Admin <r.rogah@mail.ru>
2025-10-04 00:35:21 +0300

    main.dart update

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,22 @@
 import 'package:flutter/material.dart';
+import 'theme.dart';
+import 'screens/home_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const RKMP3App());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class RKMP3App extends StatelessWidget {
+  const RKMP3App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'ФИО: Рогах Роман Владимирович',
-              style: TextStyle(fontSize: 24),
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              'Группа: ИКБО-12-22',
-              style: TextStyle(fontSize: 20),
-            ),
-            const SizedBox(height: 16),
-            const Text(
-              'Номер студенческого: 22И1769',
-              style: TextStyle(fontSize: 20),
-            ),
-          ],
-        ),
-      ),
+      title: 'RKMP3 Demo',
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      home: const HomeScreen(),
     );
   }
 }
